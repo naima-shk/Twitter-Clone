9157cbc27f65ff7df024ce3bd75e7595b4d80d9d
naima shaikh <naimashaikh550@gmail.com>
2020-01-17 23:39:27 +0500

    'adding'

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import PageWrapper from './Component/PageWrapper';
+import Home from './Component/Pages/Home';
+class  App extends Component {
+  render(){
+  return (
+    <PageWrapper>
+      <Home/>
+      </PageWrapper>
+  );
+}
+  }
+
+export default App;
---
+++ src/Component/Pages/Home.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react'
+
+class Home extends Component {
+    render(){
+    return (
+        <div>
+        <header className="masthead">
+          <div className="container">
+           <div className="intro-text">
+            <div className="intro-lead-in">Welcome To Our Studio!</div>
+              <div className="intro-heading text-uppercase">It's Nice To Meet You</div>
+             <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
+          </div>
+          </div>
+         </header>
+          </div>
+          )
+         }
+       }
+export default Home;
