26c696ff44476b831ee8b1a0477a07064c58bbf3
naima shaikh <naimashaikh550@gmail.com>
2020-01-18 00:37:07 +0500

    'adding'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React,{Component} from 'react';
 import PageWrapper from './Component/PageWrapper';
 import Home from './Component/Pages/Home';
+import {BrowserRouter as Router,Route,Link}  from 'react-router-dom';
 class  App extends Component {
   render(){
   return (
     <PageWrapper>
+      <Router>
+        <Route
+        path="/home"
+        Component={Home}
+        />
+      </Router>
       <Home/>
       </PageWrapper>
   );
