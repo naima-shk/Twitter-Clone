ab5a72df6b0f98ba2291f5e56c61fd526c9c5483
naima shaikh <naimashaikh550@gmail.com>
2020-01-21 19:45:54 +0500

    'adding'

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ class  App extends Component {
         path="/"
         Component={Home}
         />
+      <Route
+        path="/about"
+        Component={About}
+        />
       </PageWrapper>
       </Router>
   );
