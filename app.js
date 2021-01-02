86a357d829bd2205952d71f45dcdd688be59ba1e
naima <naimashaikh550@gmail.com>
2021-01-02 06:40:31 +0000

    backend

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require ('express');
 const app= express();
 const mongoose = require('mongoose');
-const db = require('./config/keys').mongoURI;
+const mongoConnectionString = require('./config/keys').mongoURI;
+//const users = require("./routes/api/users");
+//const tweets = require("./routes/api/tweets");
+//const bodyParser = require('body-parser');
 
-mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err));
+mongoose.connect(mongoConnectionString, {useNewUrlParser: true, useUnifiedTopology: true});
   
 app.get('/', (req,res) => res.send('Hello world'));
+//app.use("/api/users", users);
+//app.use("/api/tweets", tweets);
+
+
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
