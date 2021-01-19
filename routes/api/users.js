7a4e8d6401754ea256cae1a29f566578c54982e7
naima <naimashaikh550@gmail.com>
2021-01-19 15:36:44 +0500

    update

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,7 @@ const router= express.Router();
 const User= require('../../Models/User');
 const bcrypt = require('bcryptjs');
 const keys = require('../../config/keys');
+const { ExtractJwt } = require('passport-jwt');
 
 router.get('/test', (req,res) =>{
     res.json({msg:"This is a user route"});
@@ -49,7 +50,22 @@ router.post('/register', (req, res) => {
       bcrypt.compare(password, user.password)
       .then(isMatch =>{
         if(isMatch){
-          res.json({msg: "success!"});
+         const payload ={
+           id: user.id,
+           handle:user.handle,
+           email: user.email
+         }
+         jwt.sign(
+           payload,
+           keys.secretOrkey,
+           {expiresin: 3600},
+           (err,token) => {
+             res.json({
+               success: true,
+               token: "Bearer" + token
+             });
+           }
+         )
         } else{
           return res.status(400).json({password: "Incorrect password"});
         }
