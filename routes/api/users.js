11c556528f2ee01f46092d4aba217a57d29e05ed
naima <naimashaikh550@gmail.com>
2021-01-26 00:01:05 +0500

    update

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcryptjs');
 const keys = require('../../config/keys');
 const jwt  = require('jsonwebtoken');
 const passport =require ('passport');
-const ValidateRegisterInput =require('../../validation/register');
+const validateRegisterInput =require('../../validation/register');
 const ValidateLoginInput = require ('../../validation/login');
 
 router.get('/test', (req,res) =>{
@@ -19,34 +19,27 @@ router.get("/current", passport.authenticate ("jwt",{session: false}),
 ) 
 
 router.post('/register', (req, res) => {
-  const {errors, isValid} = ValidateRegisterInput(req.body);
-     return res.status(400).json(errors);
-    // Check to make sure nobody has already registered with a duplicate email
-    User.findOne({ email: req.body.email })
-      .then(user => {
-        if (user) {
-          // Throw a 400 error if the email address already exists
-          return res.status(400).json({email: "A user has already registered with this address"})
-        } else {
-          // Otherwise create a new user
-          const newUser = new User({
-            handle: req.body.handle,
-            email: req.body.email,
-            password: req.body.password
-          })
-  
-          bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(newUser.password, salt, (err, hash) => {
-              if (err) throw err;
-              newUser.password = hash;
-              newUser.save()
-                .then(user => res.json(user))
-                .catch(err => console.log(err));
-            })
-          })
-        }
-      })
-  })
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        // Use the validations to send the error
+        errors.email = 'Email already exists';
+        return res.status(400).json(errors);
+      } else {
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password
+        })
+      }
+    })
+})
 
   router.post('/login', (req,res) =>{
     const email= req.body.email;
