374ae015c3dd91b38425cecec2f2d3f1befab66c
naima <naimashaikh550@gmail.com>
2021-01-30 15:42:09 +0500

    update

--- routes/api/users.js
+++ routes/api/users.js
@@ -39,7 +39,17 @@ router.post('/register', (req, res) => {
         })
       }
     })
+  
+      bcrypt.genSalt(10, (err, salt) =>{
+        bcrypt.hash(newUser.password, salt, (err,hash)=>{
+       if(err) throw err;
+       newUser.password =hash;
+       newUser.save()
+          .then((user) => res.json(user))
+          .catch(err => comsole.log(err))
+      })
 })
+
 
   router.post('/login', (req,res) =>{
     const email= req.body.email;
@@ -73,5 +83,6 @@ router.post('/register', (req, res) => {
       })
     })
   })
+})
 
 module.exports= router;
