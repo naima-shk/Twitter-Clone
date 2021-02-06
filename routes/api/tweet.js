29328b1b72cc0ab0d38dd307e265dc8bbc5d6650
naima-shk <naimashaikh550@gmail.com>
2021-02-06 15:04:12 +0500

    'update'

--- routes/api/tweet.js
+++ routes/api/tweet.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const router= express.Router();
-
+const passport =require('passport');
+const validateTweetInput=require("../../validation/tweets");
+const Tweet =require("../../Models/Tweet");
 
 router.get('/test', (req,res) =>{
     res.json({msg:"This is a tweet route"});
 });
+
+router.post("/",
+passport.authenticate('jwt',{session:false}),
+(res,req) =>{
+    const {isValid, errors} = validateTweetInput(req.body);
+    if(!isValid){
+        return res.status(400).json(errors);
+    }
+    const newTweet = new Tweet ({
+      user:req.user.id,
+      text:req.body.text
+    });
+
+    newTweet
+    .save()
+    .then(tweet => res.json(tweet));
+})
+module.exports=router;
