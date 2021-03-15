b0b97fc37a149bbf239b3a3ac9c2927460dfd565
naima-shk <naimashaikh550@gmail.com>
2021-03-15 12:08:55 +0500

    'redux-store'

--- config/passport.js
+++ config/passport.js
@@ -1,20 +1,20 @@
-//const JwtStrategy = require('passport-jwt').Strategy;
-//const ExtractJwt = require('passport-jwt').ExtractJwt;
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const Tweet = mongoose.model('Tweet');
 const keys = require('../config/keys');
 //const passport = require('passport');
 //const passportJWT = require('passport-jwt');
-const passportJWT = require("passport-jwt");
-const JWTStrategy   = passportJWT.Strategy;
-const ExtractJWT = passportJWT.ExtractJwt;
+const passport= require("passport-jwt");
+//const JwtStrategy   = passportJWT.Strategy;
+//const ExtractJWT = passportJWT.ExtractJwt;
 const options = {};
-options.JWTFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+options.JwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 options.secretOrKey = keys.secretOrKey;
 
 module.exports = passport => {
-  passport.use(new JWTStrategy(options, (Jwt_payload, done) => {
+  passport.use(new JwtStrategy(options, (Jwt_payload, done) => {
     // This payload includes the items we specified earlier
     console.log(Jwt_payload);
     done();
